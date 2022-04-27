e757b5486d2a91aa229c1d88e181455277ff72f9
mmckendry <martinmckendry@live.ie>
2022-04-27 18:04:22 +0100

    inital commit

---
+++ genodyme-rest-service/src/test/java/com/mck/genodyme/GenodymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mck.genodyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenodymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
